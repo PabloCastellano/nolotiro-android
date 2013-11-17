d2d29c16a03529b416e677d16b74cfee47540b60
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-11-17 19:42:19 +0100

    Created base exception

---
+++ Nolotiro/src/main/java/org/alabs/nolotiro/dialogs/NolotiroException.java
@@ -0,0 +1,10 @@
+package org.alabs.nolotiro.dialogs;
+
+/**
+ * Created by zero on 11/17/13.
+ */
+public class NolotiroException extends Exception {
+    public NolotiroException(String message) {
+        super(message);
+    }
+}
