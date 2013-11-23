ccc8082f502dea9e5d1b9a6dff89f310ed73dbbd
Pablo Castellano <pablo@anche.no>
2013-11-23 21:59:39 +0100

    Improve logic of getNolotiroCacheDir() and getNolotiroDir()

--- Nolotiro/src/main/java/org/alabs/nolotiro/Utils.java
+++ Nolotiro/src/main/java/org/alabs/nolotiro/Utils.java
@@ -36,37 +36,46 @@ public class Utils {
 
     public static String getNolotiroCacheDir(Context ctx) throws NolotiroException {
         String state = Environment.getExternalStorageState();
-        String dir;
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            dir = ctx.getExternalCacheDir().toString();
+        String dir = null;
+        File f = ctx.getExternalCacheDir();
+
+        if (Environment.MEDIA_MOUNTED.equals(state) && f != null) {
+            dir = f.toString();
         } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            dir = ctx.getCacheDir().toString();
-            Log.i(TAG, "External cache storage is read only. Using internal memory.");
-            Log.i(TAG, "Internal cache storage directory is " + dir);
-        } else {
-            Log.w(TAG, "media error: state=" + state);
-            throw new NolotiroException("Error trying to get Nolotiro cache directory");
+            f = ctx.getCacheDir();
+            if (f != null) {
+                dir = f.toString();
+                Log.i(TAG, "External cache storage is read only. Using internal memory.");
+                Log.i(TAG, "Internal cache storage directory is " + dir);
+            } else {
+                Log.w(TAG, "media error: state=" + state);
+                throw new NolotiroException("Error trying to get Nolotiro cache directory");
+            }
         }
 
-        dir += File.separator + NOLOTIRO_DIR;
+        dir += File.separator + NOLOTIRO_DIR + File.separator;
         return dir;
     }
 
     public static String getNolotiroDir(Context ctx) throws NolotiroException {
         String state = Environment.getExternalStorageState();
-        String dir;
+        String dir = null;
+
         if (Environment.MEDIA_MOUNTED.equals(state)) {
             dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
         } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            dir = ctx.getFilesDir().toString();
-            Log.i(TAG, "External storage is read only. Using internal memory.");
-            Log.i(TAG, "Internal storage directory is " + dir);
-        } else {
-            Log.w(TAG, "media error: state=" + state);
-            throw new NolotiroException("Error trying to get Nolotiro directory");
+            File f = ctx.getFilesDir();
+            if (f != null) {
+                dir = f.toString();
+                Log.i(TAG, "External storage is read only. Using internal memory.");
+                Log.i(TAG, "Internal storage directory is " + dir);
+            } else {
+                Log.w(TAG, "media error: state=" + state);
+                throw new NolotiroException("Error trying to get Nolotiro directory");
+            }
         }
 
-        dir += File.separator + NOLOTIRO_DIR;
+        dir += File.separator + NOLOTIRO_DIR + File.separator;
         return dir;
     }
 
