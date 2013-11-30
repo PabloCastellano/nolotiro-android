42523d9a3ee68ebfbe8f92ed669afd6211dd8624
Pablo Castellano <pablo@anche.no>
2013-11-30 21:18:18 +0100

    New actions for detailed ad actionbar: bookmark, contact owner and post comment

--- Nolotiro/src/main/java/org/alabs/nolotiro/AdViewActivity.java
+++ Nolotiro/src/main/java/org/alabs/nolotiro/AdViewActivity.java
@@ -44,12 +44,15 @@ public class AdViewActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_bookmark:
+                boolean status = true;
+                Log.i(TAG, "Bookmark ad=" + ad.getId() + ": " + status);
+                return true;
+            case R.id.action_comment:
+                return true;
+            case R.id.action_contact:
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
