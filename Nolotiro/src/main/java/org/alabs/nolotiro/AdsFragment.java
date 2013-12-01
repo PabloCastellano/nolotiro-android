666993619d58b4123e68a1e988bb485f1c7e20d0
Pablo Castellano <pablo@anche.no>
2013-12-01 20:09:35 +0100

    Added GPLv3 headers to files

--- Nolotiro/src/main/java/org/alabs/nolotiro/AdsFragment.java
+++ Nolotiro/src/main/java/org/alabs/nolotiro/AdsFragment.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2013 "Pablo Castellano <pablo@anche.no>"
+ * Copyright (c) 2013 "Eugenio Cano-Manuel Mendoza <eugeniocanom@gmail.com>"
+ * Nolotiro App [http://nolotiro.org]
+ *
+ * This file is part of nolotiro-android.
+ *
+ * nolotiro-android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.alabs.nolotiro;
 
 import android.content.Context;
@@ -9,7 +30,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ListView;
-
 
 public class AdsFragment extends ListFragment {
 
